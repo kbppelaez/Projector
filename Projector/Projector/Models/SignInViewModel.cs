d7a0b021e387a75f2f60d87de3ffce79ae34bc7c
kbppelaez <kppelaez@up.edu.ph>
2023-09-27 21:16:19 +0800

    Created VM for Signing In

---
+++ Projector/Projector/Models/SignInViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Projector.Core.Users.DTO;
+
+namespace Projector.Models
+{
+    public class SignInViewModel
+    {
+        /* SERVICES */
+
+        /* CONSTRUCTORS */
+        public SignInViewModel() {
+            Errors = Enumerable.Empty<string>();
+        }
+
+        public SignInViewModel(UserData user, IEnumerable<string> errors)
+        {
+            User = user;
+            Errors = errors ?? Enumerable.Empty<string>();
+        }
+
+
+
+        /* PROPERTIES */
+        public UserData User { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
