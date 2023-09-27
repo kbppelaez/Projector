01898adc6af87191894e71c65dda0f992f314ce0
kbppelaez <kppelaez@up.edu.ph>
2023-09-27 21:16:01 +0800

    Created Initial Setup for ProjectDbContext

---
+++ Projector/Projector/Data/ProjectorDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Projector.Data
+{
+    public class ProjectorDbContext : DbContext
+    {
+        /* CONSTRUCTORS */
+        public ProjectorDbContext() { }
+
+        public ProjectorDbContext(DbContextOptions<ProjectorDbContext> options) : base(options) {
+        
+        }
+
+        /* PROPERTIES */
+        public DbSet<User> Users { get; set; }
+
+        /* METHODS */
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=.\SQLExpress;Database=Projector;Trusted_Connection=True;TrustServerCertificate=True;");
+            }
+        }
+    }
+}
---
+++ Projector/Projector/Data/ProjectorDbContextUsersExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+
+namespace Projector.Data
+{
+    public static class ProjectorDbContextUsersExtensions
+    {
+        public static bool UserNameExists(this DbSet<User> users, string username)
+        {
+            return users.Any(user => user.UserName == username);
+        }
+
+    }
+}
