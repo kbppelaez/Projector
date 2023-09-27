f392e44827175aae95c5caced1c9be749150b536
kbppelaez <kppelaez@up.edu.ph>
2023-09-27 22:29:29 +0800

    Created DTO and VM for the Register feature

---
+++ Projector/Projector/Core/Users/DTO/NewUserData.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projector.Core.Users.DTO
+{
+    public class NewUserData
+    {
+        /* PROPERTIES */
+        [Required]
+        [StringLength(200, MinimumLength = 5, ErrorMessage = "Please enter a valid email address.")]
+        [Display(Name = "Email Address")]
+        [EmailAddress]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(11, MinimumLength = 7, ErrorMessage = "Password should be 7-11 characters only.")]
+        [RegularExpression(@"^[!-~]{7,11}$", ErrorMessage = "Please input alphanumeric or special characters only.")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(11, MinimumLength = 7, ErrorMessage = "Password should be 7-11 characters only.")]
+        [RegularExpression(@"^[!-~]{7,11}$", ErrorMessage = "Please input alphanumeric or special characters only.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(200, MinimumLength = 2)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(200, MinimumLength = 2)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+    }
+}
---
+++ Projector/Projector/Models/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Projector.Core.Users.DTO;
+
+namespace Projector.Models
+{
+    public class RegisterViewModel
+    {
+        public RegisterViewModel()
+        {
+            Errors = Enumerable.Empty<string>();
+        }
+
+        public RegisterViewModel(NewUserData newUser, IEnumerable<string> errors)
+        {
+            this.NewUser = newUser;
+            Errors = errors ?? Enumerable.Empty<string>();
+        }
+
+        public NewUserData NewUser { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
