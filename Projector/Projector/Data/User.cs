9c117a7cc75a8d02942af7a2c7f20bfb1e441cf3
kbppelaez <kppelaez@up.edu.ph>
2023-09-27 21:08:48 +0800

    Created User Entity

--- Projector/Projector/Data/User.cs
+++ Projector/Projector/Data/User.cs
@@ -1,10 +1,28 @@
-﻿namespace Projector.Data
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Projector.Data
 {
     public class User
     {
-
         public int Id { get; set; }
+
+        [Required]
+        [Unicode(false)]
+        [StringLength(200, MinimumLength = 5)]
         public string UserName { get; set; }
+
+        [Required]
+        [Unicode(false)]
+        [StringLength(11, MinimumLength = 7)]
         public string Password { get; set; }
+
+        /*
+        public bool isVerified { get; set; }
+         
+        public string linkHash { get; set; }
+
+        public int PersonID { get; set; }
+         */
     }
 }
