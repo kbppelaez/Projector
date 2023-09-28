dc6e052c9dd49d394a85af232b140feb44c6f1d0
kbppelaez <kppelaez@up.edu.ph>
2023-09-28 14:29:25 +0800

    Added Person Data Models

---
+++ Projector/Projector/Core/Persons/PersonData.cs
@@ -0,0 +1,35 @@
+﻿using Projector.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace Projector.Core.Persons
+{
+    public class PersonData
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string LastName { get; set; }
+
+        public int UserId { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
+        public PersonData(Person person) {
+            Id = person.Id;
+            FirstName = person.FirstName;
+            LastName = person.LastName;
+            UserId = person.UserId;
+        }
+    }
+}
---
+++ Projector/Projector/Data/Person.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projector.Data
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string LastName { get; set; }
+
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
