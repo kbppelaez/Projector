4f42bc0623492eaa247bac548155360550882979
kbppelaez <kppelaez@up.edu.ph>
2023-09-29 23:40:39 +0800

    Created View Model for Create and Listing Projects

---
+++ Projector/Projector/Models/CreateProjectViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Projector.Core.Projects;
+
+namespace Projector.Models
+{
+    public class CreateProjectViewModel
+    {
+        /* CONSTRUCTORS */
+        public CreateProjectViewModel()
+        {
+            Errors = Enumerable.Empty<string>();
+        }
+
+        public CreateProjectViewModel(ProjectData project, IEnumerable<string> errors)
+        {
+            Project = project;
+            Errors = errors ?? Enumerable.Empty<string>();
+        }
+
+        /* PROPERTIES */
+        public ProjectData Project { get; set; }
+
+        public IEnumerable<string> Errors { get; set; }
+
+    }
+}
---
+++ Projector/Projector/Models/ProjectsListViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Projector.Core;
+using Projector.Core.Projects;
+using Projector.Core.Projects.DTO;
+using System.Security.Claims;
+
+namespace Projector.Models
+{
+    public class ProjectsListViewModel
+    {
+        private readonly IProjectsService _projectsService;
+
+        /* CONSTRUCTOR */
+        public ProjectsListViewModel() { }
+        public ProjectsListViewModel(IProjectsService projectsService) { 
+            _projectsService = projectsService;
+            Projects = Array.Empty<ProjectData>();
+        }
+
+        /* PROPERTIES */
+        //for Content
+        public ProjectData[] Projects { get; set; }
+        public ProjectSearchQuery Args { get; set; }
+
+        //for Pagination
+        public List<Dictionary<string,string>> PageArgs { get; set; }
+        public int TotalProjects { get; set; }
+        public int TotalPages {  get; set; }
+        public bool HasPreviousPage { get; set; }
+        public bool HasNextPage { get; set; }
+
+        /* METHODS */
+        public async Task Initialize(ProjectSearchQuery query)
+        {
+            Projects = await _projectsService.GetPersonProjects(query);
+        }
+    }
+}
