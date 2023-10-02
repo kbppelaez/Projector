8991c86e272bf114ceec7f0c47ec8cbdfb121ea7
kbppelaez <kppelaez@up.edu.ph>
2023-10-02 14:43:43 +0800

    Created Model for viewing details of an Assignment

---
+++ Projector/Projector/Models/ProjectAssignmentsViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Projector.Core;
+using Projector.Core.Persons;
+using Projector.Core.Projects;
+using Projector.Data;
+
+namespace Projector.Models
+{
+    public class ProjectAssignmentsViewModel
+    {
+        private readonly IProjectsService _projectsService;
+
+        /* CONSTRUCTORS */
+        public ProjectAssignmentsViewModel() { }
+
+        public ProjectAssignmentsViewModel(IProjectsService projectsService) {
+            _projectsService = projectsService;
+        }
+
+        /* PROPERTIES */
+        public bool Exists {  get; set; }
+        public ProjectData Details { get; set; }
+        public List<PersonData> Assignees { get; set; }
+        public List<PersonData> UnassignedEmployees { get; set; }
+
+        /* METHODS */
+        public async Task Initialize(int projectId)
+        {
+            Details = await _projectsService.GetProject(projectId);
+            if(Details == null)
+            {
+                Exists = false;
+                return;
+            }
+
+
+        }
+    }
+}
