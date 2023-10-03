ec280afde8cc0885737dc1da296612ac3ca9e09c
kbppelaez <kppelaez@up.edu.ph>
2023-10-03 10:44:47 +0800

    Added function to convert args as keyvalue pair for links

--- Projector/Projector/Core/Projects/DTO/ProjectSearchQuery.cs
+++ Projector/Projector/Core/Projects/DTO/ProjectSearchQuery.cs
@@ -4,7 +4,7 @@
     {
         public string Term { get; set; }
         public int Page { get; set; }
-        public int PageSize { get; set; }
+        public int PageSize { get; set; } = 2;
         public int PersonId { get; set; }
         public int ViewPage
         {
@@ -12,6 +12,16 @@
             {
                 return Page + 1;
             }
+        }
+
+        public Dictionary<string, string> ToStringArgument(int change)
+        {
+            return new Dictionary<string, string>
+            {
+                {"Term", this.Term },
+                {"Page", ((this.Page) + change).ToString() },
+                {"PageSize", this.PageSize.ToString()}
+            };
         }
     }
 }
