61051d62afb8665655b5df3793e09ce94042b487
kbppelaez <kppelaez@up.edu.ph>
2023-10-03 21:58:31 +0800

    Created Model for EditProject

---
+++ Projector/Projector/Models/EditProjectViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Projector.Core;
+using Projector.Core.Projects.DTO;
+
+namespace Projector.Models
+{
+    public class EditProjectViewModel
+    {
+        private readonly IProjectsService _projectsService;
+
+        public EditProjectViewModel() { }
+
+        public EditProjectViewModel(IProjectsService projectsService) {
+            _projectsService = projectsService;
+            Errors = Enumerable.Empty<string>();
+        }
+
+        public EditProjectViewModel(ProjectData project, IEnumerable<string> errors)
+        {
+            Project = project;
+            Errors = errors ?? Enumerable.Empty<string>();
+        }
+
+        public ProjectData Project { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+        public bool ProjectExists { get; set; }
+
+        public async Task Initialize(int projectId, int personId)
+        {
+            Project = await _projectsService.GetProjectDetails(projectId, personId);
+            ProjectExists = Project != null;
+        }
+    }
+}
