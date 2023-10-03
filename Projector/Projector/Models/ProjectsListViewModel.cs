56e600ad4b1ab345421c9b6e9ca82a1fcdece447
kbppelaez <kppelaez@up.edu.ph>
2023-10-03 19:35:30 +0800

    Used ProjectSearchResult class; Created function for Pagination; Shortened Long Remarks

--- Projector/Projector/Models/ProjectsListViewModel.cs
+++ Projector/Projector/Models/ProjectsListViewModel.cs
@@ -13,17 +13,16 @@ namespace Projector.Models
         public ProjectsListViewModel() { }
         public ProjectsListViewModel(IProjectsService projectsService) { 
             _projectsService = projectsService;
-            Projects = Array.Empty<ProjectData>();
+            Results = new ProjectSearchResult();
         }
 
         /* PROPERTIES */
         //for Content
-        public ProjectData[] Projects { get; set; }
+        public ProjectSearchResult Results { get; set; }
         public ProjectSearchQuery Args { get; set; }
 
         //for Pagination
         public List<Dictionary<string,string>> PageArgs { get; set; }
-        public int TotalProjects { get; set; }
         public int TotalPages {  get; set; }
         public bool HasPreviousPage { get; set; }
         public bool HasNextPage { get; set; }
@@ -31,33 +30,54 @@ namespace Projector.Models
         /* METHODS */
         public async Task Initialize(ProjectSearchQuery query)
         {
-            Projects = await _projectsService.GetPersonProjects(query);
-            TotalProjects = Projects.Length;
+            Args = query;
+            Results = await _projectsService.GetPersonProjects(query);
 
-            PageArgs = TotalProjects > 0 ?
+            LimitRemarks();
+            PageArgs = Results.TotalProjects > 0 ?
                 initializePageTracker() :
                 new List<Dictionary<string, string>>();
         }
 
+        private void LimitRemarks() {
+            if (Results.TotalProjects == 0)
+            {
+                return;
+            }
+
+            for(int i=0; i<Results.Projects.Length; i += 1)
+            {
+                if (isLengthy(Results.Projects[i].Remarks))
+                {
+                    Results.Projects[i].Remarks = Results.Projects[i].Remarks
+                                                    .Substring(0, 37) + "...";
+                }
+            }
+        }
+
+        private bool isLengthy(string remarks)
+        {
+            return !string.IsNullOrEmpty(remarks) && remarks.Length > 40;
+        }
         private List<Dictionary<string,string>> initializePageTracker()
         {
-            TotalPages = computeTotalPages(Args.PageSize, TotalProjects);
+            TotalPages = computeTotalPages(Args.PageSize, Results.TotalProjects);
             PageArgs = new List<Dictionary<string, string>>();
             int pageCount = 0;
 
-            HasPreviousPage = Args.Page == 0 ? false : true;
+            HasPreviousPage = Args.Page != 0;
             if (HasPreviousPage)
             {
                 PageArgs.Add(Args.ToStringArgument(-1)); //-1 since previous page
-            }
 
-            //buttons to the left of current page
-            for(int left = Args.Page-2; left!=Args.Page; left += 1)
-            {
-                if (left >= 0)
+                //buttons to the left of current page
+                for (int left = Args.Page - 2; left != Args.Page; left += 1)
                 {
-                    PageArgs.Add(Args.ToStringArgument(left - Args.Page));
-                    pageCount += 1;
+                    if (left >= 0)
+                    {
+                        PageArgs.Add(Args.ToStringArgument(left - Args.Page));
+                        pageCount += 1;
+                    }
                 }
             }
 
@@ -65,17 +85,17 @@ namespace Projector.Models
             PageArgs.Add(Args.ToStringArgument(0));
             pageCount += 1;
 
-            //buttons to the right of the current page
-            int i = 1;
-            while(pageCount < 5 && Args.Page+i < TotalPages)
-            {
-                PageArgs.Add(Args.ToStringArgument(i++));
-                pageCount += 1;
-            }
-
-            HasNextPage = Args.Page == TotalPages - 1 ? false : true;
+            HasNextPage = Args.Page != TotalPages - 1;
             if (HasNextPage)
             {
+                //buttons to the right of the current page
+                int i = 1;
+                while (pageCount < 5 && Args.Page + i < TotalPages)
+                {
+                    PageArgs.Add(Args.ToStringArgument(i++));
+                    pageCount += 1;
+                }
+
                 PageArgs.Add(Args.ToStringArgument(1)); //next page of current page
             }
 
@@ -85,6 +105,11 @@ namespace Projector.Models
         private int computeTotalPages(int pageSize, int totalProjects)
         {
             return (int)Math.Ceiling(totalProjects / (double)pageSize);
+        }
+
+        public int GetPageNumber(string page)
+        {
+            return int.Parse(page) + 1;
         }
     }
 }
