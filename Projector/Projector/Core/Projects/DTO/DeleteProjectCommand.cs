95e028d5572a43dce939c5a85ae356405cba1308
kbppelaez <kppelaez@up.edu.ph>
2023-10-03 22:57:34 +0800

    Created Delete Command and handler

---
+++ Projector/Projector/Core/Projects/DTO/DeleteProjectCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace Projector.Core.Projects.DTO
+{
+    public class DeleteProjectCommand
+    {
+        public DeleteProjectCommand() { }
+
+        public ProjectData Project {  get; set; }
+    }
+}
---
+++ Projector/Projector/Core/Projects/DeleteProjectCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using Projector.Core.Projects.DTO;
+using Projector.Data;
+
+namespace Projector.Core.Projects
+{
+    public class DeleteProjectCommandHandler : ICommandHandler<DeleteProjectCommand>
+    {
+        private readonly ProjectorDbContext _db;
+        private readonly IProjectsService _projectsService;
+
+        public DeleteProjectCommandHandler() { }
+
+        public DeleteProjectCommandHandler(ProjectorDbContext context, IProjectsService projectsService) {
+            _db = context;
+            _projectsService = projectsService;
+        }
+
+        public async Task<CommandResult> Execute(DeleteProjectCommand cmd)
+        {
+            if(cmd == null)
+            {
+                return CommandResult.Error("404");
+            }
+
+            //delete dependencies first
+            Project project = await _db.Projects.FindAsync(cmd.Project.Id);
+            project.Assignees.Clear();
+            _db.Projects.Update(project);
+
+            //delete project
+            _db.Projects.Remove(project);
+            await _db.SaveChangesAsync();
+
+            return CommandResult.Success(cmd.Project);
+        }
+    }
+}
