34029b4ea79fd8547c4092c3f00205dc96aaf058
kbppelaez <kppelaez@up.edu.ph>
2023-10-03 19:44:22 +0800

    Moved to Projects/DTO

---
+++ Projector/Projector/Core/Projects/DTO/ProjectDetailsData.cs
@@ -0,0 +1,15 @@
+﻿using Projector.Core.Persons.DTO;
+
+namespace Projector.Core.Projects.DTO
+{
+    public class ProjectDetailsData
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public decimal Budget { get; set; }
+        public string BudgetString { get; set; }
+        public string Remarks { get; set; }
+        public PersonData[] Assigned { get; set; }
+    }
+}
