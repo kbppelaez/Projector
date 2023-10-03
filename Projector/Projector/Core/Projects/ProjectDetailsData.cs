1a557956ee5210c9f4fe0c53b46d6a9abcd80403
kbppelaez <kppelaez@up.edu.ph>
2023-10-03 09:52:14 +0800

    Added model for Project Details

---
+++ Projector/Projector/Core/Projects/ProjectDetailsData.cs
@@ -0,0 +1,16 @@
+﻿using Projector.Core.Persons;
+
+namespace Projector.Core.Projects
+{
+    public class ProjectDetailsData
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public decimal Budget { get; set; }
+        public string BudgetString { get; set; }
+        public string Remarks { get; set; }
+        public List<PersonData> Assignees { get; set; }
+        public List<PersonData> UnassignedEmployees { get; set; }
+    }
+}
