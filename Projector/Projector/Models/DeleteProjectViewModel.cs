1f00ce2c70b31000c01425c07bba79d68c8fdb84
kbppelaez <kppelaez@up.edu.ph>
2023-10-03 22:58:10 +0800

    Added Delete View Model

---
+++ Projector/Projector/Models/DeleteProjectViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Projector.Core;
+using Projector.Core.Projects.DTO;
+
+namespace Projector.Models
+{
+    public class DeleteProjectViewModel
+    {
+        private readonly IProjectsService _projectService;
+
+        public DeleteProjectViewModel(IProjectsService projectsService) {
+            _projectService = projectsService;
+        }
+
+        public DeleteProjectViewModel(ProjectData project)
+        {
+            Project = project;
+        }
+
+        public ProjectData Project {  get; set; }
+        public bool ProjectExists { get; set; }
+
+
+        public async Task Initialize(int projectId, int personId)
+        {
+            Project = await _projectService.GetProjectDetails(projectId, personId);
+            ProjectExists = Project != null;
+        }
+    }
+}
