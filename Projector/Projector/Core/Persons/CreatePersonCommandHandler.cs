95a5c857b8ee6f172925770d8df7b9dce265f698
kbppelaez <kppelaez@up.edu.ph>
2023-10-03 09:53:14 +0800

    Created views and handler for creating a person's account

---
+++ Projector/Projector/Core/Persons/CreatePersonCommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using Projector.Core.Persons.DTO;
+using Projector.Data;
+
+namespace Projector.Core.Persons
+{
+    public class CreatePersonCommandHandler : ICommandHandler<CreatePersonCommand>
+    {
+        private readonly ProjectorDbContext _db;
+        private readonly IPersonsService _personsService;
+
+        public CreatePersonCommandHandler(ProjectorDbContext context, IPersonsService personsService)
+        {
+            _db = context;
+            _personsService = personsService;
+        }
+        public async Task<CommandResult> Execute(CreatePersonCommand args)
+        {
+            var duplicateEmail = _db.Users.UserNameExists(args.NewPerson.UserName);
+            if(duplicateEmail)
+            {
+                return CommandResult.Error("Email Address already exists.");
+            }
+
+            User newUser = new User
+            {
+                UserName = args.NewPerson.UserName,
+                Password = "temp",
+            };
+
+            // TODO
+            // Add isverified column
+            // generate temporary password
+
+            _db.Users.Add(newUser);
+            await _db.SaveChangesAsync();
+
+            Person newPerson = new Person
+            {
+                FirstName = args.NewPerson.FirstName,
+                LastName = args.NewPerson.LastName,
+                UserId = newUser.Id
+            };
+
+            _db.Persons.Add(newPerson);
+            await _db.SaveChangesAsync();
+
+            return CommandResult.Success(newPerson);
+        }
+    }
+}
---
+++ Projector/Projector/Models/CreatePersonViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Projector.Core.Persons.DTO;
+
+namespace Projector.Models
+{
+    public class CreatePersonViewModel
+    {
+        public CreatePersonViewModel()
+        {
+            Errors = Enumerable.Empty<string>();
+        }
+
+        public CreatePersonViewModel(NewPersonData newPerson, IEnumerable<string> errors)
+        {
+            NewPerson = newPerson;
+            Errors = errors ?? Enumerable.Empty<string>();
+        }
+
+        public NewPersonData NewPerson { get; set; }
+
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
