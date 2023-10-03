82ff04ee8b501fd72bbd6fe5f89620d1f32cf069
kbppelaez <kppelaez@up.edu.ph>
2023-10-03 09:41:31 +0800

    Added IsVerifiedColumn to Users

---
+++ Projector/Projector/Migrations/20231003012417_ChangeTypeOfIsVerified.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projector.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeTypeOfIsVerified : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "IsVerified",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsVerified",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
