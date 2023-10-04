8284288d3752f84bae08714142d0478fbf59e661
kbppelaez <kbppelaez@gmail.com>
2023-10-04 03:13:47 -0700

    Created View and respective View Model for Delete Person

---
+++ Projector/Projector/Models/DeletePersonViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Projector.Core;
+using Projector.Core.Persons.DTO;
+
+namespace Projector.Models
+{
+    public class DeletePersonViewModel
+    {
+        private readonly IPersonsService _personsService;
+
+        public DeletePersonViewModel() { }
+        public DeletePersonViewModel(IPersonsService personsService) {
+            _personsService = personsService;
+        }
+
+        /* PROPERTIES */
+        public PersonData Person { get; set; }
+
+        public bool PersonExists { get; set; }
+
+        /* METHODS */
+
+        public async Task Initialize(int personId)
+        {
+            Person = await _personsService.GetPerson(personId);
+            PersonExists = Person != null;
+        }
+    }
+}
