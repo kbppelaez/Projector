a41b5d87db717f0ae76f42cb89e107907f437b7d
kbppelaez <kppelaez@up.edu.ph>
2023-10-04 14:45:27 +0800

    Created View Model for the Edit View

---
+++ Projector/Projector/Models/EditPersonViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Projector.Core;
+using Projector.Core.Persons.DTO;
+
+namespace Projector.Models
+{
+    public class EditPersonViewModel
+    {
+        private readonly IPersonsService _personsService;
+
+        public EditPersonViewModel() { }
+
+        public EditPersonViewModel(IPersonsService personsService) { 
+            _personsService = personsService;
+            Errors = Enumerable.Empty<string>();
+        }
+
+        public EditPersonViewModel(NewPersonData edited, IEnumerable<string> errors)
+        {
+            EditableValues = edited;
+            Errors = errors ?? Enumerable.Empty<string>();
+        }
+
+        /* Properties */
+        public NewPersonData EditableValues { get; set; }
+
+        public IEnumerable<string> Errors { get; set; }
+
+        public bool PersonExists { get; set; }
+        public int PersonId { get; set; }
+
+        public async Task Initialize(int personId)
+        {
+            PersonData person = await _personsService.GetPerson(personId);
+            PersonExists = person != null;
+
+            if (!PersonExists)
+                return;
+
+            EditableValues = createNewPersonData(person);
+        }
+
+        private NewPersonData createNewPersonData(PersonData person)
+        {
+            return new NewPersonData
+            {
+                FirstName = person.FirstName,
+                LastName = person.LastName,
+                UserName = person.User.UserName
+            };
+        }
+    }
+}
