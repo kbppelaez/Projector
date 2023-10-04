823602602cc7ecce4159f813443e49efc9fe2918
kbppelaez <kppelaez@up.edu.ph>
2023-10-04 14:45:09 +0800

    Created Command for Edit

---
+++ Projector/Projector/Core/Persons/DTO/EditPersonCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace Projector.Core.Persons.DTO
+{
+    public class EditPersonCommand
+    {
+        public NewPersonData EditedPerson {  get; set; }
+        public int PersonId {  get; set; }
+    }
+}
