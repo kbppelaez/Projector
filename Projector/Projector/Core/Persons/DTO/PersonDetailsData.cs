bf7187b8e23468334fe5f97bb64d2330d544c5b6
kbppelaez <kbppelaez@gmail.com>
2023-10-04 04:08:01 -0700

    Created DTO, View Model, and View for View Person Details Module

---
+++ Projector/Projector/Core/Persons/DTO/PersonDetailsData.cs
@@ -0,0 +1,24 @@
+﻿using Projector.Core.Projects.DTO;
+using Projector.Core.Users.DTO;
+using System.ComponentModel.DataAnnotations;
+
+namespace Projector.Core.Persons.DTO
+{
+    public class PersonDetailsData
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public UserData User { get; set; }
+        public ProjectData[] Projects { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+    }
+}
---
+++ Projector/Projector/Models/PersonDetailsViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Projector.Core;
+using Projector.Core.Persons.DTO;
+using Projector.Core.Projects.DTO;
+
+namespace Projector.Models
+{
+    public class PersonDetailsViewModel
+    {
+        private readonly IPersonsService _personsService;
+
+        public PersonDetailsViewModel() { }
+
+        public PersonDetailsViewModel(IPersonsService personsService) {
+            _personsService = personsService;
+        }
+
+        /* PROPERTIES */
+        public PersonDetailsData Details { get; set; }
+        public bool PersonExists { get; set; }
+        public int UserPersonId { get; set; }
+
+        /* METHODS */
+        public async Task Initialize(int personId, int currId)
+        {
+            UserPersonId = currId;
+            Details = await _personsService.GetPersonWithProject(personId);
+            PersonExists = Details != null;
+        }
+    }
+}
