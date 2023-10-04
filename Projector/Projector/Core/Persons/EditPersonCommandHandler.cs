e700bacde51f194a435c9297cccad22e1414b7a9
kbppelaez <kbppelaez@gmail.com>
2023-10-04 02:19:07 -0700

    Created handler of edit person module

---
+++ Projector/Projector/Core/Persons/EditPersonCommandHandler.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projector.Core.Persons.DTO;
+using Projector.Data;
+
+namespace Projector.Core.Persons
+{
+    public class EditPersonCommandHandler : ICommandHandler<EditPersonCommand>
+    {
+        private readonly ProjectorDbContext _db;
+        private readonly IPersonsService _personsService;
+
+        public EditPersonCommandHandler() { }
+
+        public EditPersonCommandHandler(ProjectorDbContext dbContext, IPersonsService personsService) {
+            _db = dbContext;
+            _personsService = personsService;
+        }
+        public async Task<CommandResult> Execute(EditPersonCommand args)
+        {
+            Person person = _db.Persons
+                .Where(p => p.Id == args.PersonId)
+                .Include(p => p.User)
+                .FirstOrDefault();
+
+            if(person == null)
+            {
+                return CommandResult.Error("404");
+            }
+
+            if (!isSame(person.User.UserName, args.EditedPerson.UserName))
+            {
+                var duplicate = _db.Users.UserNameExists(args.EditedPerson.UserName);
+
+                if (duplicate)
+                {
+                    return CommandResult.Error("Email Address already exists.");
+                }
+                person.User.UserName = args.EditedPerson.UserName;
+            }
+
+            if (!isSame(person.FirstName, args.EditedPerson.FirstName))
+            {
+                person.FirstName = args.EditedPerson.FirstName;
+            }
+
+            if(!isSame(person.LastName, args.EditedPerson.LastName))
+            {
+                person.LastName = args.EditedPerson.LastName;
+            }
+
+            _db.Persons.Update(person);
+            await _db.SaveChangesAsync();
+
+            return CommandResult.Success(args);
+        }
+
+        private bool isSame(string s1, string s2)
+        {
+            return s1.Equals(s2);
+        }
+    }
+}
