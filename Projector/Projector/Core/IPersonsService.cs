878aac5ba0beca6e8faeb26a2a50b907fe2a73c6
kbppelaez <kppelaez@up.edu.ph>
2023-10-04 13:00:07 +0800

    Created GetPersons function that gets all persons in the datasource

--- Projector/Projector/Core/IPersonsService.cs
+++ Projector/Projector/Core/IPersonsService.cs
@@ -1,6 +1,9 @@
-﻿namespace Projector.Core
+﻿using Projector.Core.Persons.DTO;
+
+namespace Projector.Core
 {
     public interface IPersonsService
     {
+        Task<PersonSearchResult> GetPersons(PersonSearchQuery query);
     }
 }
--- Projector/Projector/Core/Persons/PersonsService.cs
+++ Projector/Projector/Core/Persons/PersonsService.cs
@@ -1,4 +1,8 @@
-﻿using Projector.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Projector.Core.Persons.DTO;
+using Projector.Core.Users.DTO;
+using Projector.Data;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Projector.Core.Users
 {
@@ -9,6 +13,42 @@ namespace Projector.Core.Users
         public PersonsService(ProjectorDbContext db)
         {
             _db = db;
+        }
+
+        public async Task<PersonSearchResult> GetPersons(PersonSearchQuery args)
+        {
+            var query = _db.Persons
+                .Where(p => !p.IsDeleted);
+
+            if (!string.IsNullOrEmpty(args.Term))
+            {
+                query = query
+                    .Where(p => p.FirstName.Contains(args.Term)
+                        || p.LastName.Contains(args.Term));
+            }
+
+            int totalPersons = query.Count();
+
+            query = query.OrderBy(p => p.LastName);
+            query = skipPersons(query, args.Page, args.PageSize);
+
+            return new PersonSearchResult
+            {
+                TotalPersons = totalPersons,
+                People = await query
+                                .Select(p => new PersonData {
+                                    Id = p.Id,
+                                    FirstName = p.FirstName,
+                                    LastName = p.LastName,
+                                    User = new UserData(p.User)
+                                })
+                                .ToArrayAsync()
+            };
+        }
+
+        private IQueryable<Person> skipPersons(IQueryable<Person> query, int page, int pageSize)
+        {
+            return query.Skip(page * pageSize).Take(pageSize);
         }
     }
 }
