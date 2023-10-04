e194739f1dc1665353b8ab1036815890f08e4e6d
kbppelaez <kppelaez@up.edu.ph>
2023-10-04 13:03:39 +0800

    Created DTOs for the searching of persons in the database

---
+++ Projector/Projector/Core/Persons/DTO/PersonSearchQuery.cs
@@ -0,0 +1,31 @@
+﻿namespace Projector.Core.Persons.DTO
+{
+    public class PersonSearchQuery
+    {
+        public PersonSearchQuery() { }
+
+        /* PROPERTIES */
+        public string Term { get; set; }
+        public int Page { get; set; }
+        public int PageSize { get; set; } = 5;
+
+        public int ViewPage
+        {
+            get
+            {
+                return Page + 1;
+            }
+        }
+
+        /* METHODS */
+        public Dictionary<string, string> ToStringArgument(int change)
+        {
+            return new Dictionary<string, string>
+            {
+                { "Term", this.Term },
+                { "Page", (this.Page + change).ToString() },
+                { "PageSize", this.PageSize.ToString() }
+            };
+        }
+    }
+}
---
+++ Projector/Projector/Core/Persons/DTO/PersonSearchResult.cs
@@ -0,0 +1,11 @@
+﻿namespace Projector.Core.Persons.DTO
+{
+    public class PersonSearchResult
+    {
+        public PersonSearchResult() { }
+
+        public PersonData[] People { get; set; }
+
+        public int TotalPersons { get; set; }
+    }
+}
