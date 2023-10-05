9b4b2d8218dd7d83d094713214260f2a992d6045
kbppelaez <kbppelaez@gmail.com>
2023-10-05 01:55:43 -0700

    Created CommandDTO and Handler for Removing Person from a Project

---
+++ Projector/Projector/Core/Projects/DTO/RemovePersonCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace Projector.Core.Projects.DTO
+{
+    public class RemovePersonCommand
+    {
+        public RemovePersonCommand() { }
+        public RemovePersonCommand(AssigneeData assigneeData)
+        {
+            PersonId = assigneeData.PersonId;
+            ProjectId = assigneeData.ProjectId;
+        }
+
+        public int PersonId { get; set; }
+
+        public int ProjectId { get; set; }
+    }
+}
---
+++ Projector/Projector/Core/Projects/RemovePersonCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Projector.Core.Projects.DTO;
+using Projector.Data;
+
+namespace Projector.Core.Projects
+{
+    public class RemovePersonCommandHandler : ICommandHandler<RemovePersonCommand>
+    {
+        private readonly ProjectorDbContext _db;
+        private readonly IProjectsService _projectsService;
+
+        public RemovePersonCommandHandler() { }
+
+        public RemovePersonCommandHandler(ProjectorDbContext db, IProjectsService projectsService)
+        {
+            _db = db;
+            _projectsService = projectsService;
+        }
+
+        public async Task<CommandResult> Execute(RemovePersonCommand args)
+        {
+            var result = await _projectsService.RemovePersonFromProject(args.PersonId, args.ProjectId);
+
+            return result == "OK" ?
+                CommandResult.Success(result) :
+                CommandResult.Error(result);
+        }
+    }
+}
