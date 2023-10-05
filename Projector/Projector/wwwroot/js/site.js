0ff99e23e88ba41931514f5dd33b1203e1f870ee
kbppelaez <kbppelaez@gmail.com>
2023-10-05 01:58:09 -0700

    Added RemoveEmployee function

--- Projector/Projector/wwwroot/js/site.js
+++ Projector/Projector/wwwroot/js/site.js
@@ -49,3 +49,50 @@ function AddEmployee(urlAction) {
         });
     }
 }
+
+function RemoveEmployee(urlAction, persId) {
+    var projId = document.getElementById("projectId").value;
+
+    if (persId == 0) {
+        document.getElementById("removeError").innerHTML = "Please select an employee."
+    } else {
+        document.getElementById("removeError").innerHTML = "";
+        var data = {
+            "projectId": projId,
+            "personId": persId
+        };
+
+        $.ajax({
+            url: urlAction,
+            contentType: 'application/json; charset=utf-8',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("XSRF-TOKEN",
+                    $('input:hidden[name="__RequestVerificationToken"]').val());
+            },
+            dataType: 'json',
+            type: 'POST',
+            headers: {
+                "Accepts": "application/json"
+            },
+            data: JSON.stringify(data),
+            success: function (response) {
+                //temp = response;
+                if (response.status == "FAIL") {
+                    document.getElementById("removeError").innerHTML = response.error;
+                } else if (response.status == "SUCCESS") {
+                    document.getElementById("Assigned").innerHTML = response.assignedView;
+                    document.getElementById("Unassigned").innerHTML = response.unassignedView;
+                }
+            },
+            error: function (response) {
+                document.getElementById("removeError").innerHTML = "An error occured."
+                temp = response;
+                //alert(response);
+            }
+        }).done(function (result) {
+            if (result.status == "REDIRECT") {
+                window.location.href = result.redirectUrl;
+            }
+        });
+    }
+}
