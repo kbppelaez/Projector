e8f077d6ef772cc6bae1c84c96548486ab8c9e63
kbppelaez <kbppelaez@gmail.com>
2023-10-05 00:57:10 -0700

    Added AssignPersonCommand constructor given an AssigneeData

--- Projector/Projector/Core/Projects/DTO/AssignPersonCommand.cs
+++ Projector/Projector/Core/Projects/DTO/AssignPersonCommand.cs
@@ -2,8 +2,15 @@
 {
     public class AssignPersonCommand
     {
+        public AssignPersonCommand() { }
+        public AssignPersonCommand(AssigneeData assigneeData) {
+            PersonId = assigneeData.PersonId;
+            ProjectId = assigneeData.ProjectId;
+        }
+
         public int PersonId { get; set; }
 
         public int ProjectId { get; set; }
+
     }
 }
