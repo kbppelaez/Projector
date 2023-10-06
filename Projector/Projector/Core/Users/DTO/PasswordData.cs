8477ff8bdbd72767e2d3775ff1f691907b8002ac
kbppelaez <kbppelaez@gmail.com>
2023-10-06 11:58:11 +0800

    Created DTO for passing data to reset password

---
+++ Projector/Projector/Core/Users/DTO/PasswordData.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projector.Core.Users.DTO
+{
+    public class PasswordData
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(11, MinimumLength = 7, ErrorMessage = "Password should be 7-11 characters only.")]
+        [RegularExpression(@"^[!-~]{7,11}$", ErrorMessage = "Please input alphanumeric or special characters only.")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(11, MinimumLength = 7, ErrorMessage = "Password should be 7-11 characters only.")]
+        [RegularExpression(@"^[!-~]{7,11}$", ErrorMessage = "Please input alphanumeric or special characters only.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Token { get; set; }
+
+        [Required]
+        public int Status { get; set; }
+
+    }
+}
---
+++ Projector/Projector/Core/Users/DTO/ResetPasswordCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace Projector.Core.Users.DTO
+{
+    public class ResetPasswordCommand
+    {
+        public PasswordData Password {  get; set; }
+        public int UserId { get; set; }
+    }
+}
