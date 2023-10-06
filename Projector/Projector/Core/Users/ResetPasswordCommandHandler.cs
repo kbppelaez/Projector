0b61640ae0944e737e7109efae892159ee1e683a
kbppelaez <kbppelaez@gmail.com>
2023-10-06 11:58:59 +0800

    Created handler for resetpassword command and view model

---
+++ Projector/Projector/Core/Users/ResetPasswordCommandHandler.cs
@@ -0,0 +1,66 @@
+﻿using Projector.Core.Users.DTO;
+using Projector.Data;
+using Projector.Migrations;
+
+namespace Projector.Core.Users
+{
+    public class ResetPasswordCommandHandler : ICommandHandler<ResetPasswordCommand>
+    {
+        private readonly ProjectorDbContext _db;
+        private readonly IUsersService _usersService;
+
+        public ResetPasswordCommandHandler() { }
+
+        public ResetPasswordCommandHandler(ProjectorDbContext context, IUsersService usersService) {
+            _db = context;
+            _usersService = usersService;
+        }
+        public async Task<CommandResult> Execute(ResetPasswordCommand args)
+        {
+            //password matches
+            if (args.Password.Password != args.Password.ConfirmPassword)
+            {
+                return CommandResult.Error("Password does not match.");
+            }
+
+            //user exists
+            if (!_usersService.UserExists(args.UserId))
+            {
+                return CommandResult.Error("INVALID");
+            }
+            
+            //verification valid?
+            if(!_usersService.VerificationValid(args.UserId, args.Password.Token))
+            {
+                return CommandResult.Error("INVALID");
+            }
+
+            return await changePassword(args);
+        }
+
+        private async Task<CommandResult> changePassword(ResetPasswordCommand args)
+        {
+            User user = _db.Users
+                .Where(u => u.Id == args.UserId)
+                .FirstOrDefault();
+
+            user.Password = _usersService.HashPassword(args.Password.Password);
+
+            if (isNewAccount(args.Password.Status))
+            {
+                user.IsVerified = true;
+            }
+            user.VerificationLink = null;
+
+            _db.Users.Update(user);
+            await _db.SaveChangesAsync();
+
+            return CommandResult.Success(args);
+        }
+
+        private bool isNewAccount(int  status)
+        {
+            return status == 1;
+        }
+    }
+}
---
+++ Projector/Projector/Models/ResetPasswordViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Projector.Core;
+using Projector.Core.Users.DTO;
+
+namespace Projector.Models
+{
+    public class ResetPasswordViewModel
+    {
+        private readonly IUsersService _usersService;
+
+        public ResetPasswordViewModel() {
+            Errors = Enumerable.Empty<string>();
+        }
+
+        public ResetPasswordViewModel(IUsersService usersService) {
+            _usersService = usersService;
+            Errors = Enumerable.Empty<string>();
+        }
+
+        public ResetPasswordViewModel(PasswordData newPassword, IEnumerable<string> errors) {
+            NewPassword = newPassword;
+            Errors = errors ?? Enumerable.Empty<string>();
+        }
+
+        public PasswordData NewPassword {  get; set; }
+        public int UserId { get; set; }
+
+        public IEnumerable<string> Errors { get; set; }
+        public bool UserExists { get; set; }
+        public bool VerificationValid {  get; set; }
+
+        public void Initialize(int userId, string token) {
+            if(!_usersService.UserExists(userId))
+            {
+                UserExists = false;
+                return;
+            }
+            UserExists = true;
+            UserId = userId;
+
+            var isNew = ! _usersService.isVerified(userId);
+
+            if (!_usersService.VerificationValid(userId, token))
+            {
+                VerificationValid = false;
+                return;
+            }
+            VerificationValid = true;
+            NewPassword = new PasswordData
+            {
+                Token = token,
+                Status = isNew ? 1 : 0,
+            };
+        }
+    }
+}
