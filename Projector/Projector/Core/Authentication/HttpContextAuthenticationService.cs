e9cca8dbc66cd3e0d10dd42aa0dc27601591572e
kbppelaez <kbppelaez@gmail.com>
2023-10-09 21:21:20 +0800

    Added Authentication modules; [UNFINISHED]

---
+++ Projector/Projector/Core/Authentication/HttpContextAuthenticationService.cs
@@ -0,0 +1,34 @@
+﻿namespace Projector.Core.Authentication
+{
+    public class HttpContextAuthenticationService : IAuthenticationService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly LinkGenerator _linkGenerator;
+
+        public HttpContextAuthenticationService(IHttpContextAccessor httpContextAccessor, LinkGenerator generator)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _linkGenerator = generator;
+        }
+
+        /*public void SignIn(UserData user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SignOut()
+        {
+            throw new NotImplementedException();
+        }*/
+
+        public string GetRoute(string actionName, string controllerName, object routeValues)
+        {
+            var path = _linkGenerator.GetPathByAction(actionName, controllerName, routeValues);
+
+            var scheme = _httpContextAccessor.HttpContext.Request.Scheme;
+            var host = _httpContextAccessor.HttpContext.Request.Host;
+
+            return scheme + "://" + host + path;
+        }
+    }
+}
---
+++ Projector/Projector/Core/IAuthenticationService.cs
@@ -0,0 +1,16 @@
+﻿namespace Projector.Core
+{
+    public interface IAuthenticationService
+    {
+        //void SignIn(UserData user);
+        //void SignOut();
+        string GetRoute(string actionName, string controllerName, object routeValues);
+    }
+    /*
+    public class UserData
+    {
+        public int PersonId { get; set; }
+        public string Username { get; set; }
+        public string FullName { get; set; }
+    }*/
+}
