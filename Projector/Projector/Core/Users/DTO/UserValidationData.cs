40202b9bb5d91b155898c2e00711d60914b0c843
kbppelaez <kbppelaez@gmail.com>
2023-10-09 21:22:14 +0800

    Created data and view for verifying user account from registration

---
+++ Projector/Projector/Core/Users/DTO/UserValidationData.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projector.Core.Users.DTO
+{
+    public class UserValidationData
+    {
+        public int UserId { get; set; }
+        public string Token { get; set; }
+
+        [Required]
+        public string Code { get; set; }
+    }
+}
---
+++ Projector/Projector/Models/VerifyUserViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Projector.Core.Users.DTO;
+using System.ComponentModel.DataAnnotations;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Projector.Models
+{
+    public class VerifyUserViewModel
+    {
+        public VerifyUserViewModel() {
+            Errors = Enumerable.Empty<string>();
+        }
+
+        public VerifyUserViewModel(UserValidationData userData, IEnumerable<string> errors)
+        {
+            SetValidationData(userData);
+            Errors = errors ?? Enumerable.Empty<string>();
+        }
+        public int UserId { get; set; }
+        public string Token { get; set; }
+
+        [Required]
+        public string Code { get; set; }
+
+        public IEnumerable<string> Errors { get; set; }
+
+        private void SetValidationData(UserValidationData data)
+        {
+            UserId = data.UserId;
+            Token = data.Token ?? string.Empty;
+            Code = data.Code ?? string.Empty;
+        }
+
+    }
+}
