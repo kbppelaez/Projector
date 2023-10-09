ce531d37761d91dca0c43870cf0089c90970ba89
kbppelaez <kbppelaez@gmail.com>
2023-10-09 13:24:34 +0800

    Added JS VM function for removing a person

--- Projector/Projector/wwwroot/js/ProjectDetailsView.js
+++ Projector/Projector/wwwroot/js/ProjectDetailsView.js
@@ -1,23 +1,28 @@
 (function ($, scope) {
 
-    function ToggleAddButton(form) {
-        $("#addPersonToProjectButton", form).attr('disabled', false);
+    function ToggleAddButton(form, disable) {
+        $("#addPersonToProjectButton", form).attr('disabled', disable);
     }
 
-    function ReInitializeAddButton() {
-        var button = document.getElementById("addPersonToProjectButton");
-        if (button) {
-            button.addEventListener("click", AddPersonToProject);
-        }
+    function ToggleRemoveButton(disable) {
+        $(".RemoveButton").attr('disabled', disable);
     }
+
     function PromptSelectError(form, msg) {
         $('#selectError', form).html(msg);
     }
 
-    function SendAddPersonRequest(form, data) {
-        ToggleAddButton(form);
-        PromptSelectError(form, ""); //clears select error span
+    function PromptRemoveError(id, msg) {
+        if (id == -1) { // -1 is passed when all removeError spans would be given msg
+            $('.RemoveError').html(msg);
+            return;
+        }
+        $('#removeError-' + id).html(msg);
+    }
 
+    function SendAddPersonRequest(form, data) {
+        ToggleAddButton(form, true);
+        PromptSelectError(form, ""); //clears select error span
         $.ajax({
             url: form.attr('action'),
             contentType: 'application/json; charset=utf-8',
@@ -32,22 +37,65 @@
             },
             data: JSON.stringify(data),
             success: function (response) {
-                temp = response;
+                //temp = response;
                 if (response.status) {
                     PromptSelectError(form, response.responseText);
-                } else {
+                }else {
                     $("#Assignments").html(response);
-                    ReInitializeAddButton();
+                    initializeView();
                 }
             },
             error: function (response) {
-                temp = response.responseText;
+                //temp = response.responseText;
                 form = $('#addPersonToProjectForm');
-                PromptSelectError(form, temp);
+                PromptSelectError(form, response.responseText);
             }
         })
-        .done(function (e) {
-            ToggleAddButton($('#addPersonToProjectForm'))
+        .done(function (response) {
+            ToggleAddButton($('#addPersonToProjectForm'), false);
+        });
+    }
+
+    function SendRemovePersonRequest(form, data) {
+        ToggleRemoveButton(true);
+        PromptRemoveError(-1, ""); //set all to empty string
+        var redirect = false;
+
+        $.ajax({
+            url: form.attr('action'),
+            contentType: 'application/json; charset=utf-8',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("XSRF-TOKEN",
+                    $('input:hidden [name="__RequestVerificationToken"]').val()
+                );
+            },
+            type: 'POST',
+            headers: {
+                "Accepts": 'text/html',
+            },
+            data: JSON.stringify(data),
+            success: function (response) {
+                //temp = response;
+                if (response.status) {
+                    PromptRemoveError(data.personId, response.responseText);
+                } else if (response.startsWith("/")) {
+                    redirect = true;
+                } else {
+                    $("#Assignments").html(response);
+                    initializeView();
+                }
+            },
+            error: function (response) {
+                //temp = response.responseText;
+                form = $('#Remove-' + data.personId);
+                PromptRemoveError(data.personId, response.responseText);
+            }
+        }).done(function (response) {
+            if (redirect) {
+                window.location.href = response;
+            } else {
+                ToggleRemoveButton(false);
+            }
         });
     }
 
@@ -66,13 +114,34 @@
         }
     }
 
+    function RemovePersonFromProject(e) {
+        if (e.target && e.target.tagName == 'BUTTON') {
+            var personId = e.target.value;
+            var form = $('#Remove-' + personId);
+            var projectId = $('#projectId-' + personId).val();
+
+            var data = { "personId": personId, "projectId": projectId };
+            SendRemovePersonRequest(form, data);
+        }
+    }
+
     function initializeAddPersonToProject() {
-        document.getElementById("addPersonToProjectButton").addEventListener("click", AddPersonToProject);
+        var button = document.getElementById("addPersonToProjectButton");
+        if (button) {
+            button.addEventListener("click", AddPersonToProject);
+        }
+    }
+
+    function initializeRemovePersonFromProject() {
+        var buttons = $(".RemoveButton");
+        if (buttons && buttons.length > 0) {
+            buttons.on("click", RemovePersonFromProject);
+        }
     }
 
     function initializeView() {
         initializeAddPersonToProject();
-        //initializeRemovePersonFromProject();
+        initializeRemovePersonFromProject();
     }
 
     var vm = {
