e2cfbeab1c94ea9abe771a32bed2c1454eaa91b1
kbppelaez <kbppelaez@gmail.com>
2023-10-09 16:31:59 +0800

    Added sending of email

--- Projector/Projector/Core/Users/ForgotPasswordCommandHandler.cs
+++ Projector/Projector/Core/Users/ForgotPasswordCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Projector.Core.Email;
 using Projector.Core.Users.DTO;
 using Projector.Data;
 
@@ -8,12 +9,14 @@ namespace Projector.Core.Users
     {
         private readonly ProjectorDbContext _db;
         private readonly IUsersService _usersService;
+        private readonly IEmailService _emailService;
 
         public ForgotPasswordCommandHandler() { }
 
-        public ForgotPasswordCommandHandler(ProjectorDbContext context, IUsersService usersService) {
+        public ForgotPasswordCommandHandler(ProjectorDbContext context, IUsersService usersService, IEmailService emailService) {
             _db = context;
             _usersService = usersService;
+            _emailService = emailService;
         }
 
         public async Task<CommandResult> Execute(ForgotPasswordCommand args)
@@ -27,6 +30,7 @@ namespace Projector.Core.Users
             User existingUser = _db.Users
                 .Where(u => u.UserName == email)
                 .Include(u => u.Person)
+                .Include(u => u.VerificationLink)
                 .FirstOrDefault();
 
             if(existingUser == null || existingUser.Person.IsDeleted)
@@ -34,16 +38,47 @@ namespace Projector.Core.Users
                 return CommandResult.Error("User does not exist.");
             }
 
-            existingUser.VerificationLink = _usersService
-                .GenerateVerificationLink(email, existingUser.Id);
+            if(existingUser.VerificationLink == null)
+            {
+                existingUser.VerificationLink = _usersService
+                    .GenerateVerificationLink(email, existingUser.Id, 1);
+            }
+            else
+            {
+                var temporaryLink = _usersService
+                .GenerateVerificationLink(email, existingUser.Id, 1);
+
+                existingUser.VerificationLink.ActivationToken = temporaryLink.ActivationToken;
+                existingUser.VerificationLink.ActivationLink = temporaryLink.ActivationLink;
+                existingUser.VerificationLink.ExpiryDate = temporaryLink.ExpiryDate;
+            }
 
             _db.Users.Update(existingUser);
             await _db.SaveChangesAsync();
 
             //TODO
             //SEND EMAIL HERE
+            SendEmail(existingUser);
 
             return CommandResult.Success(email);
+        }
+
+        private void SendEmail(User existingUser)
+        {
+            EmailContentData emailContentData = new EmailContentData
+            {
+                Recipient = existingUser.UserName,
+                Subject = "Reset Password Link"
+            };
+
+            var content = "<h3>Reset Password Link</h3><br/>";
+            content += "<p>To reset your password, please clink this link: ";
+            content += "<a href='" + existingUser.VerificationLink.ActivationLink + "'>Reset Password</a>.</p>";
+            content += "<p>If you did not request this reset password link, kindly disregard this email.</p>";
+
+            emailContentData.Content = content;
+
+            _emailService.SendEmail(emailContentData);
         }
     }
 }
