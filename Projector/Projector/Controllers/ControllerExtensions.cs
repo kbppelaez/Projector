c4797fd7ebe491fbb55b9e4f7c3f9a208466cdfc
kbppelaez <kbppelaez@gmail.com>
2023-10-19 13:59:27 +0800

    Created extension file for extension methods for all controllers

---
+++ Projector/Projector/Controllers/ControllerExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using System.Text;
+
+namespace Projector.Controllers
+{
+    public static class ControllerExtensions
+    {
+        public static string GetRouteAbsoluteUrl(this ControllerBase controllerBase, string action, string controller, object routeValues = null)
+        {
+            var url = new StringBuilder();
+            url.Append(controllerBase.Request.Scheme + "://");
+            url.Append(controllerBase.Request.Host.Host);
+            url.Append(controllerBase.Url.Action(action, controller, routeValues));
+
+            return url.ToString();
+        }
+    }
+}
