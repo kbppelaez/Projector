1f5a7bf1093dfd5373e8d00eb929b2dfcee96a67
kbppelaez <kbppelaez@gmail.com>
2023-10-19 14:01:08 +0800

    Moved some functions to the helper class

--- Projector/Projector/Core/IUsersService.cs
+++ Projector/Projector/Core/IUsersService.cs
@@ -5,16 +5,9 @@ namespace Projector.Core
 {
     public interface IUsersService
     {
-        bool VerifyPassword(string hashed, string input);
-        string HashPassword(string password);
-        Task PersistLogin(PersonData user);
-        Task RemoveLogin();
-        string GenerateActivationToken(string email, DateTime now);
         bool UserExists(int userId);
         bool VerificationLinkValid(int id, string token);
         bool VerificationTokenValid(int id, string token);
-        bool isVerified(int userId);
-        VerificationLink GenerateVerificationLink(string userName, int id, int type);
         bool VerifyRegistration(int id, string link, string subcode);
         User GetUser(int userId);
     }
